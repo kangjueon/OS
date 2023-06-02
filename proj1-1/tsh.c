1d01a7f22e41c807162480bb09fb9db9d5185fb4
kangjueon <jueon611@gmail.com>
2023-06-02 19:39:49 +0900

    proj1

---
+++ proj1-1/tsh.c
@@ -0,0 +1,209 @@
+/*2017013663 강주언. simple shell*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+#define MAX_LINE 80             /* 명령어의 최대 길이 */
+
+/*
+ * cmdexec - 명령어를 파싱해서 실행한다.
+ * 스페이스와 탭을 공백문자로 간주하고, 연속된 공백문자는 하나의 공백문자로 축소한다. 
+ * 작은 따옴표나 큰 따옴표로 이루어진 문자열을 하나의 인자로 처리한다.
+ * 기호 '<' 또는 '>'를 사용하여 표준 입출력을 파일로 바꾸거나,
+ * 기호 '|'를 사용하여 파이프 명령을 실행하는 것도 여기에서 처리한다.
+ */
+static void cmdexec(char *cmd)
+{
+    char *argv[MAX_LINE/2+1];   /* 명령어 인자를 저장하기 위한 배열 */
+    int argc = 0;               /* 인자의 개수 */
+    char *p, *q;                /* 명령어를 파싱하기 위한 변수 */  
+    
+     //char cwd[1024];            /*open() 경로를 위한 선언. getcwd*/
+    int fd[2]={-1,-1};
+    /*
+     * 명령어 앞부분 공백문자를 제거하고 인자를 하나씩 꺼내서 argv에 차례로 저장한다.
+     * 작은 따옴표나 큰 따옴표로 이루어진 문자열을 하나의 인자로 처리한다.
+     */
+    p = cmd; p += strspn(p, " \t");
+    do {
+        /*
+         * 공백문자, 큰 따옴표, 작은 따옴표가 있는지 검사한다.
+         */ 
+        q = strpbrk(p, " \t\'\"");
+        /*
+         * 공백문자가 있거나 아무 것도 없으면 공백문자까지 또는 전체를 하나의 인자로 처리한다.
+         */
+        if (q == NULL || *q == ' ' || *q == '\t') {
+            q = strsep(&p, " \t");
+            if (*q) argv[argc++] = q;
+        }
+        /*
+         * 작은 따옴표가 있으면 그 위치까지 하나의 인자로 처리하고, 
+         * 작은 따옴표 위치에서 두 번째 작은 따옴표 위치까지 다음 인자로 처리한다.
+         * 두 번째 작은 따옴표가 없으면 나머지 전체를 인자로 처리한다.
+         */
+        else if (*q == '\'') {
+            q = strsep(&p, "\'");
+            if (*q) argv[argc++] = q;
+            q = strsep(&p, "\'");
+            if (*q) argv[argc++] = q;
+        }
+        /*
+         * 큰 따옴표가 있으면 그 위치까지 하나의 인자로 처리하고, 
+         * 큰 따옴표 위치에서 두 번째 큰 따옴표 위치까지 다음 인자로 처리한다.
+         * 두 번째 큰 따옴표가 없으면 나머지 전체를 인자로 처리한다.
+         */
+        else if (*q == '\"'){
+            q = strsep(&p, "\"");
+            if (*q) argv[argc++] = q;
+            q = strsep(&p, "\"");
+            if (*q) argv[argc++] = q;
+        } 
+
+	/*
+            '<', '>'가 있으면, 인자로 처리하고 표준입출력을 실행한다.
+
+	*/
+        else if (*q == '<'){
+            q = strsep(&p, "<");
+            if (*q) argv[argc++] = q;
+
+             if (argv[argc]) {   // < 다음에 있는 인자를 파일로 받아들임.
+        	fd[0] = open(argv[argc], O_RDONLY);   // 파일 디스크립터에 표준 입력을 저장
+        	if (fd[0] == -1) {
+           	  perror("open");
+           	  exit(1);
+        	} 
+       		dup2(fd[0], 0);   // redirect standard input to fdin
+        	close(fd[0]);
+        	argc++; 
+ 	     }         
+        } 
+        else if (*q == '>'){
+            q = strsep(&p, ">");
+            if (*q) argv[argc++] = q;
+	   
+    	   if (argv[argc]){
+            fd[1] = open(argv[argc], O_WRONLY|O_CREAT|O_TRUNC, 0644);   //표준 출력. 파일이 없다면 파일을 생성한
+            if (fd[1] == -1) {
+                perror("open");
+                exit(1);
+            	}
+              dup2(fd[1], 1);   // redirect standard input to fdout
+              close(fd[1]);
+              argc++; 
+      	    }         
+        }
+    } while (p);
+
+    argv[argc] = NULL;
+
+    // Check for input and output redirection
+    if (fd[0] != 0) {
+        dup2(fd[0], 0);
+        close(fd[0]);
+    }
+    if (fd[1] != 1) {
+        dup2(fd[1], 1);
+        close(fd[1]);
+    }
+
+    // Fork a child process
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        exit(1);
+    } else if (pid == 0) {
+        // Child process
+        execvp(argv[0], argv);
+        perror("execvp");
+        exit(1);
+    } else {
+        // Parent process
+        wait(NULL);
+    }
+}
+
+
+/*
+ * 기능이 간단한 유닉스 셸인 tsh (tiny shell)의 메인 함수이다.
+ * tsh은 프로세스 생성과 파이프를 통한 프로세스간 통신을 학습하기 위한 것으로
+ * 백그라운드 실행, 파이프 명령, 표준 입출력 리다이렉션 일부만 지원한다.
+ */
+int main(void)
+{
+    char cmd[MAX_LINE+1];       /* 명령어를 저장하기 위한 버퍼 */
+    int len;                    /* 입력된 명령어의 길이 */
+    pid_t pid;                  /* 자식 프로세스 아이디 */
+    int background;             /* 백그라운드 실행 유무 */
+    
+    /*
+     * 종료 명령인 "exit"이 입력될 때까지 루프를 무한 반복한다.
+     */
+    while (true) {
+        /*
+         * 좀비 (자식)프로세스가 있으면 제거한다.
+         */
+        pid = waitpid(-1, NULL, WNOHANG);
+        if (pid > 0)
+            printf("[%d] + done\n", pid);
+        /*
+         * 셸 프롬프트를 출력한다. 지연 출력을 방지하기 위해 출력버퍼를 강제로 비운다.
+         */
+        printf("tsh> "); fflush(stdout);
+        /*
+         * 표준 입력장치로부터 최대 MAX_LINE까지 명령어를 입력 받는다.
+         * 입력된 명령어 끝에 있는 새줄문자를 널문자로 바꿔 C 문자열로 만든다.
+         * 입력된 값이 없으면 새 명령어를 받기 위해 루프의 처음으로 간다.
+         */
+        len = read(STDIN_FILENO, cmd, MAX_LINE);
+        if (len == -1) {
+            perror("read");
+            exit(EXIT_FAILURE);
+        }
+        cmd[--len] = '\0';
+        if (len == 0)
+            continue;
+        /*
+         * 종료 명령이면 루프를 빠져나간다.
+         */
+        if(!strcasecmp(cmd, "exit"))
+            break;
+        /*
+         * 백그라운드 명령인지 확인하고, '&' 기호를 삭제한다.
+         */
+        char *p = strchr(cmd, '&');
+        if (p != NULL) {
+            background = 1;
+            *p = '\0';
+        }
+        else
+            background = 0;
+        /*
+         * 자식 프로세스를 생성하여 입력된 명령어를 실행하게 한다.
+         */
+        if ((pid = fork()) == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+        /*
+         * 자식 프로세스는 명령어를 실행하고 종료한다.
+         */
+        else if (pid == 0) {
+            cmdexec(cmd);
+            exit(EXIT_SUCCESS);
+        }
+        /*
+         * 포그라운드 실행이면 부모 프로세스는 자식이 끝날 때까지 기다린다.
+         * 백그라운드 실행이면 기다리지 않고 다음 명령어를 입력받기 위해 루프의 처음으로 간다.
+         */
+        else if (!background)
+            waitpid(pid, NULL, 0);
+    }
+    return 0;
+}
